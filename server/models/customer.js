fc1b11e6e9852077aa6bb2b323e8cb5b1b2e8ee8
Shubham Singh <imshubhamsingh97@gmail.com>
2018-09-24 17:22:52 +0530

    added logic for latest customer

--- server/models/customer.js
+++ server/models/customer.js
@@ -1,5 +1,18 @@
 'use strict';
 
 module.exports = function(Customer) {
-
+  /**
+   * Function for fetching latest customer added to datasource
+   */
+  Customer.latest = function(callback) {
+    Customer.find(
+      {
+        order: 'id DESC',
+        limit: 1
+      },
+      (err, user) => {
+        callback(null, ...user);
+      }
+    );
+  };
 };
