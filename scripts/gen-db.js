391346673dea47448c84e326de734f6eeee63a9a
Shubham Singh <imshubhamsingh97@gmail.com>
2018-09-25 08:11:00 +0530

    added db generation script

---
+++ scripts/gen-db.js
@@ -0,0 +1,23 @@
+/**
+ * @file File creates new db file.
+ * @author Shubham Singh
+ */
+
+const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+
+const DB_FOLDER_PATH = path.join(__dirname, '..', 'server/db');
+const DB_PATH = path.join(DB_FOLDER_PATH, 'data.txt');
+
+if (fs.existsSync(DB_PATH)) {
+  process.stdout.write(
+    chalk.yellow.bold('📄  Existing DB data found.') +
+      chalk.dim(' It will be replaced with a new one\n')
+  );
+  fs.unlinkSync(DB_PATH);
+}
+
+process.stdout.write(chalk.green.bold('   ↪ ✅  Success!\n'));
+fs.writeFileSync(DB_PATH, '');
+process.exit(0);
--- server/server.js
+++ server/server.js
@@ -2,18 +2,20 @@
 
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
-var app = module.exports = loopback();
+var chalk = require('chalk');
+var app = (module.exports = loopback());
 
 app.start = function() {
   // start the web server
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
+    process.stdout.write(chalk.magenta(`💻  API started on ${baseUrl}/api`));
     if (app.get('loopback-component-explorer')) {
       var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+      process.stdout.write(
+        chalk.magenta(`\n📖  API Documentation on ${baseUrl}/${explorerPath}\n`)
+      );
     }
   });
 };
@@ -24,6 +26,5 @@ boot(app, __dirname, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+  if (require.main === module) app.start();
 });
